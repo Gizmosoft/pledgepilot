576955539f2b4e627782d0530a8dc26d2be6cff2
kartikeyhebbar <vijayakumarhebbar.k@northeastern.edu>
2023-11-20 23:11:01 -0500

    updated routes

--- src/app/routes/campaign-routes.js
+++ src/app/routes/campaign-routes.js
@@ -9,7 +9,7 @@ router.route('/create')
     .post(campaignController.addCampaign)   // call addCampaign function in controller
 
 // '/campaigns/:campaign-name' GetCampaign API to get a particular API
-router.route('/campaigns/:campaignName')
+router.route('/:campaignName')
     .get(campaignController.getCampaign)
 
 export default router
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,5 +2,5 @@ import campaignRouter from './campaign-routes.js'
 
 // define endpoints related to campaign model
 export default (app) => {
-    app.use('/', campaignRouter)
+    app.use('/campaigns', campaignRouter)
 }
