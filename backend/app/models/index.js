6ea927934921ed1f413c6b5ff4e9fa6f348faa9f
DeepanshChaturvedi <chaturvedi.de@northeastern.edu>
2023-12-08 10:46:01 -0500

    Added blog routes, added in index. Created Blog tsx

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,10 +1,10 @@
 import Campaign from './Campaign.js';
 import User from './User.js';
 import Community from './community.js'
-import Blogs from './Blog.js'
+import Blog from './Blog.js'
 export default {
     Campaign,
     User,
     Community,
-    Blogs
+    Blog
 }
