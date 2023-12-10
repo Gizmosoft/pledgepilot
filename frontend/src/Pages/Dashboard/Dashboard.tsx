fb06e565edcfec9d895110aa64d4817a9b3039dd
kartikeyhebbar <vijayakumarhebbar.k@northeastern.edu>
2023-12-10 17:30:22 -0500

    verified payments and rewards backend

--- frontend/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ function Dashboard() {
             <br />
             <br />
             <div>
-                <h5>Welcome {profile.firstName + ' ' + profile.lastName}</h5>
+                <h3>Welcome {profile.firstName + ' ' + profile.lastName}</h3>
                 <h6>Your email is: {profile.emailAddress}</h6>
             </div>
         </div>
