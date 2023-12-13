3814cfe4ada117cffe4e2f1dd8e6b2a866305305
kartikeyhebbar <vijayakumarhebbar.k@northeastern.edu>
2023-12-12 21:50:54 -0500

    fixed bug in milestone stats

---
+++ backend/app/models/Milestone.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const Milestone = new Schema({
+    campaignId: 
+
+})
+
+const MilestoneModel = mongoose.model("Milestone", Milestone)
+
+export default MilestoneModel
