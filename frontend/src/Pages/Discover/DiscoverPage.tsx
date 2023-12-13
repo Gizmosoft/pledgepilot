100349c043a30cd5407a9b4957cda74754a807b6
Kartik Vijay Shanbhag <shanbhag.k@northeastern.edu>
2023-12-12 21:12:27 -0500

    Search Functionality Added to Previously Created SearchBar

--- frontend/src/Pages/Discover/DiscoverPage.tsx
+++ frontend/src/Pages/Discover/DiscoverPage.tsx
@@ -7,9 +7,24 @@ const DiscoverPage = () => {
     // create a state for campaigns
     const [campaigns, setCampaigns] = useState<any>()
 
+
+    // function to get required campaigns corresponding to the search results
+
+    const handleCampaign = (arr: any[], search_field: any) => {
+        const result = arr.filter(item => item.name === search_field);
+        console.log(JSON.stringify(result));
+        if (result != null) {
+            setCampaigns(result);
+        }else {
+            setCampaigns("");
+
+        }
+    }
+
+
     // onpageload operations
     useEffect(() => {
-        const fetchAllCampaigns = async() => {
+        const fetchAllCampaigns = async () => {
             // call Discover API
             // const campaignsResponse = await fetch('http://localhost:3001/campaigns/discover')
             const campaignsData = await discoverCampaign();
@@ -20,27 +35,26 @@ const DiscoverPage = () => {
             setCampaigns(campaignsData)
         }
         fetchAllCampaigns()
-    // below empty array is called dependecy list which tells about how many times the useEffect needs to be called. Not defining this will cause an infinte loop of useEffects being called. Empty array signifies that call useEffect only once at 
+        // below empty array is called dependecy list which tells about how many times the useEffect needs to be called. Not defining this will cause an infinte loop of useEffects being called. Empty array signifies that call useEffect only once at 
     }, [])
 
-    if(!campaigns) {
+    if (!campaigns) {
         return <p>No campaigns in the database!</p>
     }
-  return (
-    <div className='discover-page'>
-        <h3>Showing Campaigns on PledgePilot</h3>
-        {/* <Search /> */}
-        <div className='card-container'>
-            {
-                campaigns.map((campaign: any) => <CampaignTile key={campaign._id} campaignObject={campaign}/>)
-            }
-        </div>
-        {/* Grid to show the campaigns in UI */}
-        {/* <div className='campaigns-grid'>
+    return (
+        <div className='discover-page'>
+            <Search campaigns={campaigns} getmyCampaign={handleCampaign} />
+            <div className='card-container' id='content-display'>
+                {
+                    campaigns.map((campaign: any) => <CampaignTile key={campaign._id} campaignObject={campaign} />)
+                }
+            </div>
+            {/* Grid to show the campaigns in UI */}
+            {/* <div className='campaigns-grid'>
             { campaigns.map((campaign: { _id: any; name: any; }) => <CampaignTile key={campaign._id} campaignId={campaign._id} campaignName={campaign.name} />)}
-        </div> */}       
-    </div>
-  )
+        </div> */}
+        </div>
+    )
 }
 
 export default DiscoverPage
