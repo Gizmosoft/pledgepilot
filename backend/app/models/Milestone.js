8e8ede78679e669f0a76db4e5a6a59d2aef3259b
kartikeyhebbar <vijayakumarhebbar.k@northeastern.edu>
2023-12-12 21:54:57 -0500

    added Milestone model

--- backend/app/models/Milestone.js
+++ backend/app/models/Milestone.js
@@ -3,8 +3,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Milestone = new Schema({
-    campaignId: 
-
+    campaignId: {
+        type: Schema.Types.ObjectId,
+        ref: "Campaign",
+    },
+    target: {
+        type: Number,
+        required: true
+    },
+    received: {
+        type: Number,
+        default: 0
+    }
+},
+{
+    versionKey: false
 })
 
 const MilestoneModel = mongoose.model("Milestone", Milestone)
